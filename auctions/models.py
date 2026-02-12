dd82f8eb3cf9cc2606623073fd8eef4f3c3fe2f0
Marcyline Guthrie <desertfruit.max@gmail.com>
2026-02-12 14:56:35 -0800

    first commit- working

---
+++ auctions/models.py
@@ -0,0 +1,30 @@
+from datetime import date
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+
+
+class User(AbstractUser):
+    pass
+
+class Listing(models.Model):
+    name = models.CharField(max_length=64)
+    discription = models.CharField(max_length=512)
+    imgURL = models.CharField(max_length=256, default="https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExeHNoZGl1azZ0dm9razJxbnBjNnd3bXc5Y2V2cmxvajl0bHkyYTV1cSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/v6aOjy0Qo1fIA/giphy.gif")
+    poster = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
+    active = models.BooleanField(max_length=1, default=False)
+    
+    
+    activeUntil = models.DateTimeField(max_length=20, blank=True, default=date.today())
+
+    def __str__(self):
+        return f"{self.name}"
+
+class Bid(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="userBets")
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name="betsOnListing")
+    price = models.DecimalField(max_digits=15, decimal_places=2)
+
+    def __str__(self):
+        return f"Bid {self.id}"
